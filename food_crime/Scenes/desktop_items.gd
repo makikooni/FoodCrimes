4bb48a2a3c6c66645b41445d78f48e747d7ccd02
Weronika E. Falinska <w.e.falinska@gmail.com>
2024-12-27 19:08:37 +0000

    CalendarFunctionality

--- food_crime/Scenes/desktop_items.gd
+++ food_crime/Scenes/desktop_items.gd
@@ -12,7 +12,11 @@ extends Node
 @onready var hint_1: Label = %Hint1
 @onready var email_1: TextureRect = $Email/emaillayout/Email1
 @onready var email_1_button: Button = %Email1Button
+@onready var email: TextureRect = $Email/Email
+@onready var open_calendar: TextureRect = %OpenCalendar
+@onready var close_calendar_button: Button = $Calendar/OpenCalendar/CloseCalendarButton
 
+var hint1_available = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,7 +26,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_files_button_pressed() -> void:
 	print("Files clicked")
@@ -44,32 +47,45 @@ func _on_database_button_pressed() -> void:
 
 func _on_audio_button_pressed() -> void:
 	print("Audio clicked")
-
-
-
+	
+#Email funtions:
 func _on_emailexitbutton_pressed() -> void:
 	print("Email clicked")
 	emaillayout.visible = false
 	email_adress.visible = false
 	email_text.visible = false
-	await get_tree().create_timer(2).timeout
-	hint_1.show_hint("Date...")
-	# Wait for 3 seconds
-	await get_tree().create_timer(3).timeout
-	hint_1.hide_hint()
-	
-
-func _on_calendar_button_pressed() -> void:
-	print("Calendar clicked")
-
-
+	if hint1_available == true:
+		await get_tree().create_timer(2).timeout
+		hint_1.show_hint("Date...")
+		# Wait for 3 seconds
+		await get_tree().create_timer(3).timeout
+		hint1_available = false
+		hint_1.hide_hint()
 func _on_email_1_button_pressed() -> void:
 	#if first time then show first email
 	email_1.visible = false
-	email_1.texture = preload("res://Assets/Emails/email_none.png")  # Change texture to closed email.
+	email_1.texture = preload("res://Assets/Emails/email_none.png")
+	print("Email1 tecxture changed to closed")  # Change texture to closed email.
 	email_adress.visible = true
 	email_text.visible = true
 	email_text.text = "I hope by now you have familiarised yourself with  your new gadget. It is time to move on and start  working on your first case.  We don’t have any time to loose. You will find all necessary files on your desktop. Key is today's date."
 	email_adress.text = "From: B.O.\nDate: 1st March 1995\nSubject: Welcome"
+	new_email_texture()
 	#TO-DO: when clicked must disable all other buttons on desktop
-	#TO-DO: add an inbox screen
+func new_email_texture():
+	#TO-DO: check for all emails inside inbox instead
+	email_1.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	#TO-DO: not perfect solution, the initial texture is not tall enough
+	if email_1.texture == preload("res://Assets/Emails/email_none.png"):
+		print("Main email icon changed to none")
+		email.texture = preload("res://Assets/Emails/email_none.png")
+	else:
+		email.texture = preload("res://Assets/Desktop/email.png")
+		print("Main email icon changed to new email.")
+
+#Calendar functions
+func _on_calendar_button_pressed() -> void:
+	print("Calendar clicked")
+	open_calendar.visible = true
+func _on_close_calendar_button_pressed() -> void:
+	open_calendar.visible = false
