afcd63d6e1bcaaed73794e08abc80b00a234935a
Weronika E. Falinska <w.e.falinska@gmail.com>
2025-01-01 02:04:00 +0000

    database_profiles

--- food_crime/Scenes/database_node.gd
+++ food_crime/Scenes/database_node.gd
@@ -1,18 +1,49 @@
 extends Node
 @onready var database_texture: TextureRect = %database_texture
 @onready var exit_button: Button = %exit_button
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+@onready var profile_n: Button = %profile_n
+@onready var profile_l: Button = %profile_l
+@onready var profile_g: Button = %profile_g
+@onready var profile_d: Button = %profile_d
+@onready var medical: Button = %medical
+@onready var newspaper: Button = %newspaper
+@onready var staff_records: Button = %staff_records
+@onready var profile_open: TextureRect = %profile_open
+@onready var password_edit: LineEdit = $database_texture/database_main/password_edit
+@onready var submit_password: Button = $database_texture/database_main/submit_password
+@onready var database_main: TextureRect = $database_texture/database_main
+@onready var database_open: TextureRect = $database_texture/database_open
 
 func _on_exit_button_pressed() -> void:
-	database_texture.visible = false
+	profile_open.visible = false
+	database_open.visible = false
+	database_main.visible = true
+	database_texture.visible = false 
+	
+func _on_submit_password_pressed() -> void:
+	if password_edit.text == "AX3521":
+		database_main.visible = false
+		database_open.visible = true 
+
+
+func _on_profile_n_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("res://Assets/Desktop/Database/nurse_profile.png")
+func _on_profile_l_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("res://Assets/Desktop/Database/lady_profile.png")
+func _on_profile_g_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("res://Assets/Desktop/Database/grandpa_profile.png")
+func _on_profile_d_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("res://Assets/Desktop/Database/daughter_profile.png")
+func _on_medical_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("")
+func _on_newspaper_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("")
+func _on_staff_records_pressed() -> void:
+	profile_open.visible = true
+	profile_open.texture = load("")
